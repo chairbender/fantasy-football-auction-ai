213d62821c87ac54bad2871e8e389d032380bd2b
kwhipke1 <kwhipke1@gmail.com>
2017-12-30 18:27:02 -0800

    trying a simpler model

--- scratchpad.py
+++ scratchpad.py
@@ -5,16 +5,20 @@ import numpy as np
 import gym
 import gym_fantasy_football_auction
 
-
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten
 from keras.optimizers import Adam
-from rl.agents import DQNAgent
+from rl.agents import DQNAgent, CEMAgent
+from rl.callbacks import Callback
 
-from rl.memory import SequentialMemory
+from rl.memory import SequentialMemory, EpisodeParameterMemory
 from rl.policy import BoltzmannQPolicy
 
-ENV_NAME = 'FantasyFootballAuction-2OwnerSmallRosterSimpleScriptedOpponent-v0'
+class Checker(Callback):
+    def on_episode_end(self, episode, logs={}):
+        self.env.render(mode='human')
+
+ENV_NAME = 'FantasyFootballAuction-2OwnerSingleRosterSimpleScriptedOpponent-v0'
 #ENV_NAME = 'CartPole-v0'
 
 
@@ -31,33 +35,49 @@ obs_dim = env.observation_space.shape
 #obs_dim = env.observation_space.shape[0]
 
 model = Sequential()
+model = Sequential()
 model.add(Flatten(input_shape=(1,env.observation_space.shape)))
-model.add(Dense(16))
-model.add(Activation('relu'))
-model.add(Dense(16))
-model.add(Activation('relu'))
-model.add(Dense(16))
-model.add(Activation('relu'))
 model.add(Dense(nb_actions))
-model.add(Activation('linear'))
+model.add(Activation('softmax'))
 print(model.summary())
 
 
 # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
 # even the metrics!
-memory = SequentialMemory(limit=50000, window_length=1)
-policy = BoltzmannQPolicy()
-dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
-               target_model_update=1e-2, policy=policy, batch_size=10, train_interval=10)
-dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+#memory = SequentialMemory(limit=50000, window_length=1)
+#policy = BoltzmannQPolicy()
+# dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=1,
+#                target_model_update=1e-2, policy=policy, batch_size=10, train_interval=10)
+# dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+#
+# # Okay, now it's time to learn something! We visualize the training here for show, but this
+# # slows down training quite a lot. You can always safely abort the training prematurely using
+# # Ctrl + C.
+# dqn.fit(env, nb_steps=50000, verbose=1)
+#
+# # After training is done, we save the final weights.
+# dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+#
+# # Finally, evaluate our algorithm for 5 episodes.
+# dqn.test(env, nb_episodes=5, visualize=False, callbacks=[Checker()])
+
+memory = EpisodeParameterMemory(limit=1000, window_length=1)
+
+cem = CEMAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=0,
+               batch_size=1, train_interval=1, elite_frac=0.05)
+cem.compile()
+
+
+# After training is done, we save the best weights.
+cem.load_weights('cem_{}_params.h5f'.format(ENV_NAME))
 
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-dqn.fit(env, nb_steps=50000)
+cem.fit(env, nb_steps=100000, visualize=False, verbose=1)
 
-# After training is done, we save the final weights.
-dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+# After training is done, we save the best weights.
+cem.save_weights('cem_{}_params.h5f'.format(ENV_NAME), overwrite=True)
 
 # Finally, evaluate our algorithm for 5 episodes.
-dqn.test(env, nb_episodes=5, visualize=True)
+cem.test(env, nb_episodes=5, visualize=False, callbacks=[Checker()])
