80f5a6d39a6972b9c964925bbebb691637c3ab85
kwhipke1 <kwhipke1@gmail.com>
2018-01-02 03:07:17 -0800

    more messing around with it, done for the night

--- scratchpad.py
+++ scratchpad.py
@@ -36,7 +36,7 @@ class LogTesting(Callback):
     def make_fig(self):
         plt.scatter(self.x, self.y)
 
-    def __init__(self, every):
+    def __init__(self, every, max=200):
         super().__init__()
         self.wincount = 0
         self.turncount = 1
@@ -44,6 +44,7 @@ class LogTesting(Callback):
         self.y = [0.]
         self.i = 1
         self.every = every
+        self.max = max
         plt.ion()
 
     def on_episode_end(self, episode, logs={}):
@@ -51,6 +52,9 @@ class LogTesting(Callback):
             self.wincount += 1
         self.turncount += 1
         if self.turncount % self.every == 0:
+            if self.i >= self.max:
+                del self.x[0]
+                del self.y[0]
             self.x.append(self.i)
             self.y.append(self.wincount / self.turncount)
             drawnow(self.make_fig)
@@ -80,7 +84,6 @@ obs_dim = env.observation_space.shape
 ### CEM Version
 def do_cem():
     model = Sequential()
-    model = Sequential()
     model.add(Flatten(input_shape=(1,env.observation_space.shape)))
     model.add(Dense(nb_actions))
     model.add(Activation('softmax'))
@@ -88,19 +91,22 @@ def do_cem():
 
     memory = EpisodeParameterMemory(limit=1000, window_length=1)
 
-    cem = CEMAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=0,
-                   batch_size=1, train_interval=1, elite_frac=0.05)
+    cem = CEMAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=2000,
+                   batch_size=512, train_interval=512, elite_frac=0.05)
     cem.compile()
     # After training is done, we save the best weights.
-    cem.load_weights('cem_{}_params.h5f'.format(ENV_NAME))
+    cem.load_weights('cem_FF_params.h5f'.format(ENV_NAME))
+
     # Okay, now it's time to learn something! We visualize the training here for show, but this
     # slows down training quite a lot. You can always safely abort the training prematurely using
     # Ctrl + C.
-    cem.fit(env, nb_steps=100000, visualize=False, verbose=1, callbacks=[ObservationChecker()])
-    # After training is done, we save the best weights.
-    cem.save_weights('cem_{}_params.h5f'.format(ENV_NAME), overwrite=True)
-    # Finally, evaluate our algorithm for 5 episodes.
-    cem.test(env, nb_episodes=5, visualize=False, callbacks=[Checker()])
+    log = LogTesting(1000)
+    while True:
+        cem.fit(env, nb_steps=1000, verbose=0, visualize=False, callbacks=[log])
+        # no need to really test atm since the sim is pretty determnistic for a given set of weights
+        # dqn.test(env, nb_episodes=100, verbose=0, visualize=False)
+        #
+        cem.save_weights('cem_FF_params.h5f'.format(ENV_NAME), overwrite=True)
 
 ### DQN Version
 def do_dqn():
@@ -121,7 +127,6 @@ def do_dqn():
     # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
     # even the metrics!
     memory = SequentialMemory(limit=1000, window_length=1)
-    policy = BoltzmannQPolicy()
 
     # Notes for myself.
     # How this works - it plays the game to get experiences. The actual training runs by sampling from those experiences
@@ -137,18 +142,25 @@ def do_dqn():
     # batch size - during training, it gets a sample of this size from the experience memory, calculates the error for
     #   all the examples in that batch, then updates the model.
     #   BTW One pass through the entire set of data (the memory) is called an epoch.
-    dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=1000,enable_dueling_network=True,
-                   target_model_update=1e-2, policy=policy, batch_size=64, train_interval=32)
+    dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=1000, enable_dueling_network=True,
+                   target_model_update=1e-2, policy=BoltzmannQPolicy(), batch_size=128, train_interval=1)
     dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
-    #dqn.load_weights('dqn_{}_params.h5f'.format(ENV_NAME))
+    dqn.load_weights('dqn_FF_params.h5f'.format(ENV_NAME))
 
     # Okay, now it's time to learn something! We visualize the training here for show, but this
     # slows down training quite a lot. You can always safely abort the training prematurely using
     # Ctrl + C.
-    log = LogTesting(100)
-    #for i in range(100):
-    dqn.fit(env, nb_steps=50000, verbose=1, visualize=False, callbacks=[log])
+    log = LogTesting(1000)
+    while True:
+        dqn.fit(env, nb_steps=1000, verbose=0, visualize=False, callbacks=[log])
+        # no need to really test atm since the sim is pretty determnistic for a given set of weights
+        #dqn.test(env, nb_episodes=100, verbose=0, visualize=False)
+        #
+        dqn.save_weights('dqn_FF_params.h5f'.format(ENV_NAME), overwrite=True)
 
 
+#do_cem()
 do_dqn()
+
+plt.show()
